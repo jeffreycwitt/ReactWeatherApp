cded8e5f7c36ef9acbd482c3d8ca83ade653feff
Jeffrey C. Witt <jeffreycwitt@gmail.com>
2016-05-27 08:25:11 -0400

    updated ErrorModal to use component props in error component

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -4,17 +4,22 @@ var ErrorModal = React.createClass({
 	getDefaultProps: function(){
 		return {
 			title: "Error"
-		}
+		};
+	},
+	propTypes: {
+		title: React.PropTypes.string,
+		message: React.PropTypes.string.isRequired
 	},
 	componentDidMount: function(){
 		var modal = new Foundation.Reveal($("#error-modal"));
 		modal.open();
 	},
 	render: function(){
+		var {title, message} = this.props;
 		return(
 			<div id="error-modal" className="reveal tiny text-center" data-reveal="">
-				<h4>Some title</h4>
-				<p>Error message</p>
+				<h4>{title}</h4>
+				<p>{message}</p>
 				<p>
 					<button className="button hollow" data-close="">Okay</button>	
 				</p>
--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -49,7 +49,7 @@ var Weather = React.createClass({
 			
 			if (typeof errorMessage === 'string'){
 				return (
-					<ErrorModal/>
+					<ErrorModal message={errorMessage}/>
 				)
 			}
 		}
